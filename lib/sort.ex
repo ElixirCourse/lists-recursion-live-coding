47eb9f268e516f0821a15a018333052c69f20db5
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-02-27 11:47:15 +0200

    Add sort tests

--- lib/sort.ex
+++ lib/sort.ex
@@ -1,4 +1,20 @@
 defmodule List.Sort do
+  @doc ~s"""
+  Sorts any lsit
+
+  ## Examples:
+      iex> List.Sort.quicksort([])
+      []
+
+      iex> List.Sort.quicksort([2])
+      [2]
+
+      iex> List.Sort.quicksort([1,2,3,3,2,-1,-10,20,100,-2])
+      [-10, -2, -1, 1, 2, 2, 3, 3, 20, 100]
+
+      iex> List.Sort.quicksort([2,1,2, -1.0, 1.0, :a, "asd", 'as', %{a: 2}, [1,2,3]])
+      [-1.0, 1, 1.0, 2, 2, :a, %{a: 2}, [1, 2, 3], 'as', "asd"]
+  """
   def quicksort([]), do: []
 
   def quicksort([pivot | t]) do
--- test/recursion_test.exs
+++ test/recursion_test.exs
@@ -2,8 +2,5 @@ defmodule RecursionTest do
   use ExUnit.Case
   doctest Recursion
   doctest List.Flatten
-
-  test "greets the world" do
-    assert Recursion.hello() == :world
-  end
+  doctest List.Sort
 end
